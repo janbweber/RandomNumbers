9249d21c7043298c6d301aca2ffe2aa523849073
Jan Weber <jay.moena@gmail.com>
2016-05-16 11:33:04 +0200

    Initial Commit

---
+++ RandomNumbers/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RandomNumbers
+//
+//  Created by Jan B Weber on 16.05.16.
+//  Copyright © 2016 Hilbert Games. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ RandomNumbers/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RandomNumbers
+//
+//  Created by Jan B Weber on 16.05.16.
+//  Copyright © 2016 Hilbert Games. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
